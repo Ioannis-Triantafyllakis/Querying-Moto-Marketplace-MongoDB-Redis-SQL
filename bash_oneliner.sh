b8d7d9218638d2e3c8f1e8c01f7b89907ac1fa0b
Ioannis Triantafyllakis <103419096+Ioannis-Triantafyllakis@users.noreply.github.com>
2022-04-14 16:44:31 +0300

    Add files via upload

---
+++ bash_oneliner.sh
@@ -0,0 +1,5 @@
+# Open the file "BIKES" through a Unix terminal, and run the command below
+find * | grep json > files_list.txt
+
+# This will construct a text file named "files_text.txt", 
+# which will include all the json files with the motorcycle listings
